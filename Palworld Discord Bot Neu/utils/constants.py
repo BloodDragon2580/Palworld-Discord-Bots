9552c098c50ca58e749bcd76a266ec326d953d6a
BloodDragon2580 <gewinner.thomas@gmail.com>
2024-08-12 18:58:22 +0200

    Update

--- Palworld Discord Bot Neu/utils/constants.py
+++ Palworld Discord Bot Neu/utils/constants.py
@@ -2,3 +2,11 @@
 FOOTER_IMAGE = "https://i.ibb.co/wYj1j7P/T-Umihebi-Fire-icon-normal.png"
 FOOTER_TEXT = "Created by BloodDragon"
 TITLE_URL = "https://discord.gg/legion-des-chaos"
+PALBOT_ART = r"""
+__________        .__ ___.           __   
+\______   \_____  |  |\_ |__   _____/  |_ 
+ |     ___/\__  \ |  | | __ \ /  _ \   __\
+ |    |     / __ \|  |_| \_\ (  <_> )  |  
+ |____|    (____  /____/___  /\____/|__|  
+                \/         \/             
+"""
